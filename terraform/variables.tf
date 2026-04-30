07d2a6995d0df7c8132cc3c6a5ac4ad82e331c91
maksimovann <148694721+maksimovann@users.noreply.github.com>
2026-04-30 20:19:26 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,32 @@ variable "zone" {
   type    = string
   default = "ru-central1-a"
 }
+
+variable "image_id" {
+  type = string
+}
+
+variable "ssh_public_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "db_name" {
+  type    = string
+  default = "redmine"
+}
+
+variable "db_user" {
+  type    = string
+  default = "redmine"
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "certificate_id" {
+  type      = string
+  sensitive = true
+}
