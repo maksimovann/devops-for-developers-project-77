4b84186e4f76364bbbc875bb85c635651a285bb4
maksimovann <148694721+maksimovann@users.noreply.github.com>
2026-04-30 20:39:47 +0300

    Update datadog.tf

--- terraform/datadog.tf
+++ terraform/datadog.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    datadog = {
-      source = "DataDog/datadog"
-    }
-  }
-}
-
 provider "datadog" {
   api_key = var.datadog_api_key
   app_key = var.datadog_app_key
