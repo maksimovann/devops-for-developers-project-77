4704820f63031e8259d1bb53ed183b651982cc44
maksimovann <148694721+maksimovann@users.noreply.github.com>
2026-04-30 20:16:18 +0300

    Create provider.tf

---
+++ terraform/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
+
+provider "yandex" {
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
+}
